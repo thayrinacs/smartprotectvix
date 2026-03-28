28aed01a16b8be2af5146089816aa043aeb3d74e
thayrinacs <thayrinacs@gmail.com>
2026-03-28 00:56:00 -0300

    removendo config node

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Feedbacks from "./pages/Feedbacks";
 
 const queryClient = new QueryClient();
 
